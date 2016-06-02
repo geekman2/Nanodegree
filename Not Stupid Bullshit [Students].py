062f591c338a617988366a29fa66fe33b9f8aa3f
geekman2 <geekmann2@gmail.com>
2016-06-02 13:51:17 -0700

    Stuff and Things

--- Not Stupid Bullshit [Students].py
+++ Not Stupid Bullshit [Students].py
@@ -55,76 +55,73 @@ def preprocess_features(X):
 X_all = preprocess_features(X_all)
 #print "Processed feature columns ({}):-\n{}".format(len(X_all.columns), list(X_all.columns))
 
-# First, decide how many training vs test samples you want
-num_all = student_data.shape[0]  # same as len(student_data)
-num_train = 300  # about 75% of the data
-num_test = num_all - num_train
+def train_test_split(num_train):
+    num_all = student_data.shape[0]  # same as len(student_data)
+    X_all = X_all.sample(frac=1).reset_index(drop=True)
+    num_test = num_all - num_train
 
-# TODO: Then, select features (X) and corresponding labels (y) for the training and test sets
-# Note: Shuffle the data or randomly select samples to avoid any bias due to ordering in the dataset
-student_data.reindex(np.random.permutation(student_data.index))
-X_train = X_all[0:num_train]
-y_train = y_all[0:num_train]
-X_test = X_all[num_train:]
-y_test = y_all[num_train:]
-print "Training set: {} samples".format(X_train.shape[0])
-print "Test set: {} samples".format(X_test.shape[0])
-# Note: If you need a validation set, extract it from within training data
+    # TODO: Then, select features (X) and corresponding labels (y) for the training and test sets
+    # Note: Shuffle the data or randomly select samples to avoid any bias due to ordering in the dataset
+    X_train = X_all[0:num_train] #X_all.sample(frac = frac_train)
+    y_train = y_all[0:num_train] #y_all.sample(frac = frac_train)#
+    X_test = X_all.sample(frac = 0.3)#X_all[300:]
+    y_test = y_all.sample(frac = 0.3)#y_all[300:]
+    print "Training set: {} samples".format(X_train.shape[0])
+    print "Test set: {} samples".format(X_test.shape[0])
+    return X_train,y_train,X_test,y_test
+
+
 
 
 # Train a model
 import time
-from sklearn.metrics import make_scorer, f1_score
-from sklearn import grid_search
 
-
-def train_classifier(clf, X_train, y_train,parameters):
+def train_classifier(clf, X_train, y_train):
     print "Training {}...".format(clf.__class__.__name__)
     start = time.time()
-    # clf.fit(X_train, y_train)
-    # Set up the parameters we wish to tune
-
-    f1_scorer = make_scorer(f1_score, pos_label="yes")
-
-    # Make the GridSearchCV object
-    clsf = grid_search.GridSearchCV(clf, parameters, scoring=f1_scorer)
-
-    # Fit the learner to the data to obtain the optimal model with tuned parameters
-    clsf.fit(X_train, y_train)
-
+    clf.fit(X_train, y_train)
     end = time.time()
-    return clsf
     print "Done!\nTraining time (secs): {:.3f}".format(end - start)
 
 
 # TODO: Choose a model, import it and instantiate an object
-from sklearn import tree, svm,naive_bayes
-from sklearn.metrics import f1_score
+from sklearn import tree, svm, naive_bayes, ensemble, neighbors
 
 dtc = tree.DecisionTreeClassifier()
 svc = svm.SVC()
 nbc = naive_bayes.GaussianNB()
+knn = neighbors.KNeighborsClassifier()
+rfc = ensemble.RandomForestClassifier()
+adc = ensemble.AdaBoostClassifier()
+
+models = [dtc,svc,nbc,knn,rfc,adc]
 
 
 # Fit model to training data
-dtc = train_classifier(dtc, X_train, y_train,
-                       parameters={"min_samples_split":(range(1,20))})
-svc = train_classifier(svc, X_train, y_train,
-                       parameters={"C":(10.0,100.0,1000.0),"degree":(range(5,20)),
-                                    "gamma":(10.0,100.0,1000.0)})# note: using entire training set here
-#dtc = dtc.fit(X_train,y_train)
-#svc = svc.fit(X_train,y_train)
-nbc = nbc.fit(X_train,y_train)
 
-#grid_predict = clf.predict(X_test)
-dtc_predict = dtc.predict(X_test)
-svc_predict = svc.predict(X_test)
-nbc_predict = nbc.predict(X_test)
 
-#print "Grid Search F1 Score",f1_score(y_test,grid_predict,pos_label="yes")
-print "Decision Tree F1 score ",f1_score(y_test,dtc_predict,pos_label="yes")
-print dtc.best_params_
-print "SVM F1 score ",f1_score(y_test,svc_predict,pos_label="yes")
-print svc.best_score_
-print "Naive Bayes F1 score ",f1_score(y_test,nbc_predict,pos_label="yes")
-# print clf  # you can inspect the learned model by printing it
+# note: using entire training set here
+
+# Predict on training set and compute F1 score
+from sklearn.metrics import f1_score
+
+def predict_labels(clf, features, target):
+    print "Predicting labels using {}...".format(clf.__class__.__name__)
+    start = time.time()
+    y_pred = clf.predict(features)
+    end = time.time()
+    print "Done!\nPrediction time (secs): {:.3f}".format(end - start)
+    return f1_score(target.values, y_pred, pos_label='yes')
+
+def train_predict(clf, X_train, y_train, X_test, y_test):
+    print "------------------------------------------"
+    print "Training set size: {}".format(len(X_train))
+    train_classifier(clf, X_train, y_train)
+    print "F1 score for training set: {}".format(predict_labels(clf, X_train, y_train))
+    print "F1 score for test set: {}".format(predict_labels(clf, X_test, y_test))
+
+
+for clf in models:
+    for x in [100,200,300]:
+        X_train,y_train,X_test,y_test = train_test_split(x)
+        train_predict(clf,X_train,y_train,X_test,y_test)
